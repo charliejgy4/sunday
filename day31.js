28d1b6a1204a032ff3d369d27020b9533397950b
charliejgy4 <103393876+charliejgy4@users.noreply.github.com>
2022-10-14 12:43:31 +0100

    Add files via upload

---
+++ day31.js
@@ -0,0 +1,92 @@
+// const cars = [ " charlie", "joseph", "beverly"];
+// console.log ("names".length);
+
+
+// let name =[
+// "charlie = Joseph",
+// "joseph "= "belverly",
+// "beverly"= "jerry"
+// ]
+// name [2]="charlie- beverly"
+// console.log (names)
+
+
+// let coffeeOrder =[
+//     " rachel- earl gray",
+//     "geoff- cartodo",
+//     "sara- latte"
+// ]
+// coffeeOrder.push("seb-greentea")
+// console.log (" seb-greentea" )
+
+
+// const cars = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"];
+
+// let text = "";
+// for (let i = 0; i < cars.length; i++) {
+//   text += cars[i];
+// }
+//  console.log ("const cars")
+
+
+
+// let songs =[
+// "gospel",
+// "soca",
+// "regga"
+// ]
+
+// songs.push ("songs")
+// songs.push( "adel")
+
+// songs.pop ()
+// console.log (songs)
+
+// let drinks =[
+//     "tea",
+//     "apple",
+//     "pear"
+//     ]
+// for ( let i = 0; i< drinks.length; i++){
+    
+//     console.log (drinks[i])
+// }
+
+
+// let num=8
+// if (num%5==0 && num % 3==0){
+//     console.log ("divisible by 5  buzz")
+// } else if( num% 5==0){
+//     console.log("fizz")
+// } else if( num% 3==0){
+//     console.log("fizz")
+// }else{
+//     console.log(num +" ....boooo.....")
+// }
+
+
+// let evenNumbers = []
+
+// for(let i = 0; i < 50; i++){
+//     if (i % 5 == 0){
+//         evenNumbers.push (i)
+//     }
+
+// }
+// console.log(evenNumbers)
+// while loop
+
+
+
+
+
+// let cards = [ "diamond", "spade","heart","club",]
+// let currentCard = "club"
+
+// while ( currentCard != "spade"){
+
+// console.log(currentCard)
+// currentCard  = cards[ Math.floor(Math.random()*4)]
+// }
+
+// console.log(currentCard)
